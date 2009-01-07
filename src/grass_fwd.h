64dfadcd03623d6472d0edfe5b99b2f0857bbbd0
yoh2 <yoh2@93f7d26a-f0f6-482b-aec6-b797ce29e8a8>
2009-01-07 13:02:01 +0000

    前回コミット時のコミットし忘れ分。
    git-svn-id: http://yoh2.ddo.jp/svn/grass/trunk@7 93f7d26a-f0f6-482b-aec6-b797ce29e8a8

---
+++ src/grass_fwd.h
@@ -0,0 +1,25 @@
+/* $Id$ */
+/*! \file
+ * \brief Grass関連型の前方宣言集
+ *
+ * \date 2009-01-07
+ * \author yoh2
+ * $LastChangedBy$
+ * $LastChangedDate$
+ */
+
+#ifndef grass_fwd_H_
+#define grass_fwd_H_
+
+/* grass_value 関連 */
+struct grass_value;
+struct grass_value_node;
+
+/* grass_instruction 関連 */
+struct grass_instraction;
+struct grass_instraction_node;
+
+/* grass_machine 関連 */
+struct grass_machine;
+
+#endif /* grass_fwd_H_ */
